0972fde16404126a00a2817de130ec53f14da161
Matthias Pfeil <matthias.pfeil@wwu.de>
2022-09-01 15:53:34 +0200

    Text changes and new pages

--- map.js
+++ map.js
@@ -13,10 +13,50 @@ map.on("style.load", () => {
   map.setFog({}); // Set the default atmosphere style
 });
 
+function goToOnMap (game) {
+
+  if (game === 'obst') {
+    map.flyTo({
+      center: [7.290480442988532, 51.9771282193672],
+      zoom: 19,
+      duration: 2000,
+      essential: true,
+    });
+  } else if (game === 'kraeuter') {
+    map.flyTo({
+      center: [7.290539367548513, 51.972629696540494],
+      zoom: 19,
+      duration: 2000,
+      essential: true,
+    });
+  }
+}
+
 // create the popup
 const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-  "<h6>einLaden Billerbeck</h6>"
+  "<div><h6>einLaden Billerbeck</h6><ul><li>Obst-Ralley</li></ul></div>"
 );
+
+const popupAFG1 = new mapboxgl.Popup({ offset: 25 }).setHTML(
+  "<h6>Wetterstation 1</h6>"
+);
+
+const popupAFG2 = new mapboxgl.Popup({ offset: 25 }).setHTML(
+  "<h6>Wetterstation 2</h6>"
+);
+
+const popupAFG3 = new mapboxgl.Popup({ offset: 25 }).setHTML(
+  "<h6>Wetterstation 3</h6>"
+);
+
+const popupAFG4 = new mapboxgl.Popup({ offset: 25 }).setHTML(
+  "<h6>Wetterstation 4</h6>"
+);
+
+const popupRundgang1 = new mapboxgl.Popup({ offset: 25 }).setHTML(
+  "<h6>Kräuter-Ralley</h6>"
+);
+
 // create DOM element for the marker
 const el = document.createElement('div');
 el.id = 'marker';
@@ -30,20 +70,29 @@ const marker1 = new mapboxgl.Marker()
 // openSenseMap / AFG senseBox
 const afg1 = new mapboxgl.Marker({ color: "#2b908f" })
   .setLngLat([7.289686, 51.978748])
-  .setPopup(popup)
+  .setPopup(popupAFG1)
   .addTo(map);
 
 const afg2 = new mapboxgl.Marker({ color: "#008FFB" })
   .setLngLat([7.285652, 51.973461])
-  .setPopup(popup)
+  .setPopup(popupAFG2)
   .addTo(map);
 
 const afg3 = new mapboxgl.Marker({ color: "#f9ce1d" })
   .setLngLat([7.293892, 51.976422])
-  .setPopup(popup)
+  .setPopup(popupAFG3)
   .addTo(map);
 
 const afg4 = new mapboxgl.Marker({ color: "#f9a3a4" })
   .setLngLat([7.287712, 51.972615])
-  .setPopup(popup)
+  .setPopup(popupAFG4)
+  .addTo(map);
+
+const rundgang1 = new mapboxgl.Marker({ color: "#0000FF"})
+  .setLngLat([7.290539367548513, 51.972629696540494])
+  .setPopup(popupRundgang1)
+  .addTo(map);
+
+const bikes = new mapboxgl.Marker({ color: "red" })
+  .setLngLat([7.297196969341912, 51.98141154947279])
   .addTo(map);
